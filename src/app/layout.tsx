fe422472333ad8061b271b895f2047e0e2b1cc50
Mohamed Abdelrahman <m7md0a.work@gmail.com>
2023-12-30 15:39:46 +0200

    add data attr in html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout({
   };
 
   return (
-    <html lang="en" >
+    <html lang="en" data-theme="light">
         {/* <body className={inter.className + ' min-h-screen bg-base-200'}> */}
         <body className="min-h-screen bg-base-200">
         <div id='modal-root'></div>
